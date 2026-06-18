294410ee62d04e70f42a637b4093ccae4ad3755b
Diana <mada.chirica@gmail.com>
2026-06-18 19:39:36 +0100

    Changed type to set

--- src/main.tf
+++ src/main.tf
@@ -94,9 +94,9 @@ resource "aws_security_group" "web" {
 
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.al2023.id
-  instance_typ           = var.instance_type
+  instance_type           = var.instance_type
   subnet_id              = data.aws_subnets.default.ids[0]
-  vpc_security_group_ids = aws_security_group.web.id
+  vpc_security_group_ids = [aws_security_group.web.id]
   iam_instance_profile   = aws_iam_instance_profile.ec2.name
 
   user_data = <<-EOT
