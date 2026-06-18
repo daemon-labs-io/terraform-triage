4502ac1ba82678e2f0b2c51998aa310c5416d23e
Diana <mada.chirica@gmail.com>
2026-06-18 20:18:08 +0100

    Changed port to 80

--- src/main.tf
+++ src/main.tf
@@ -73,8 +73,8 @@ resource "aws_security_group" "web" {
 
   ingress {
     description = "HTTP from anywhere"
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -94,7 +94,7 @@ resource "aws_security_group" "web" {
 
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.al2023.id
-  instance_type           = var.instance_type
+  instance_type          = var.instance_type
   subnet_id              = data.aws_subnets.default.ids[0]
   vpc_security_group_ids = [aws_security_group.web.id]
   iam_instance_profile   = aws_iam_instance_profile.ec2.name
